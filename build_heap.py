517beef3045cd673c9fcf9425bfb1c7411a587d9
andrismart <118056672+andrismart@users.noreply.github.com>
2023-03-19 19:00:25 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,13 +1,35 @@
-# python3
+# python 3
+import numpy as np
 
 
-def build_heap(data):
+def build_heap(data, n):
     swaps = []
-    # TODO: Creat heap and heap sort
+    # TODO: Create heap and heap sort
     # try to achieve  O(n) and not O(n2)
+    count=0
+    for i in range(n//2, -1, -1):
+        sift(i, n, data, count, swaps)
+
+    return count, swaps
 
 
-    return swaps
+
+def sift(i, n, data, count, swaps):
+    t=i
+    ls=(t+1)*2-1
+
+    if ls<=n-1 and data[ls]<data[t]:
+        t=ls
+
+    rs=(t+1)*2
+    if rs<=n-1 and data[rs]<data[t]:
+        t=rs
+
+    if not i==t:
+        data[i], data[t]=data[t], data[i]
+        count+=1
+        swaps.append((i, t))
+        sift(t, n, data, count, swaps)
 
 
 def main():
@@ -15,18 +37,27 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
+    entry = input()
+    if "I" in entry:
+        n = int(input())
+        data = list(map(int, input().split()))
+
+    if "F" in entry:
+        filepath = "test/" + input()
+        if not "a" in filepath:
+            with open(filepath, "r") as f:
+                n = int(f.readline().strip())
+                data = np.array(list(map(int, f.readline().split())))
 
 
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
     # calls function to assess the data 
     # and give back all swaps
-    swaps = build_heap(data)
+    count, swaps = build_heap(data, n)
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
